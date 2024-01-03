d85c8ed9283a8d232397f0b80f50aaa47b86bc53
financyeol <151622822+financyeol@users.noreply.github.com>
2024-01-03 19:51:37 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,23 +1,42 @@
 #1차 시도
 
+#SELECT date_format(datetime,"%h") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#FROM animal_outs
+#GROUP BY HOUR
+#ORDER BY 1 ASC;
 # 01시부터 12시까지만 나옴
 
-#2차 시도 
-
+#2차 시도
+#SELECT date_format(datetime,"%H") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#FROM animal_outs
+#WHERE HOUR >= 9
+#GROUP BY HOUR
+#ORDER BY 1 ASC;
 
 #3차시도
+#SELECT date_format(datetime,"%H") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#FROM animal_outs
+#WHERE date_format(datetime,"%H") >= 9
+#GROUP BY HOUR
+#ORDER BY 1 ASC;
 
-#아마 숫자형시기 09로 표시되어서 그런 듯?
-
-#4차시도 
-
+#4차시도
+#SELECT date_format(datetime,"%H") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#FROM animal_outs
+#WHERE HOUR >= 9 AND HOUR <20
+#GROUP BY HOUR
+#ORDER BY 1 ASC;
 
 #5차시도
-SELECT date_format(datetime,"%H") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
-FROM animal_outs
-WHERE date_format(datetime,"%H") >= 9 AND date_format(datetime,"%H") <20
-GROUP BY HOUR
-ORDER BY 1 ASC;
+#SELECT date_format(datetime,"%H") AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+#FROM animal_outs
+#WHERE date_format(datetime,"%H") >= 9 #AND date_format(datetime,"%H") <20
+#GROUP BY HOUR
+#ORDER BY 1 ASC;
 
-# 각 시간대별 입양 발생 건수 
-# 시간대 별로 정렬
+#다른 방법 
+SELECT HOUR(datetime) AS HOUR, COUNT(ANIMAL_ID) AS COUNT
+FROM animal_outs
+GROUP BY HOUR
+HAVING HOUR >= 9 AND HOUR <20
+ORDER BY 1 ASC;
