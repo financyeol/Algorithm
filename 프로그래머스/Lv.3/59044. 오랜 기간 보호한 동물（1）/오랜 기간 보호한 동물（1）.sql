950c25777adf27f653fcb0df1410aee0ee5aa7c3
financyeol <151622822+financyeol@users.noreply.github.com>
2024-01-13 16:00:09 +0900

    [level Lv.3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/Lv.3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/Lv.3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -9,3 +9,7 @@ ON i.animal_id = o.animal_id
 WHERE o.datetime is null
 ORDER BY i.datetime ASC
 LIMIT 3;
+
+# 1. 처음엔 조인하고 입양안간 친구를 어떻게 분류할까 고민
+# 2. 구글링으로 차집합 개념 (ON에 걸지, WHERE에 걸지 찾아봄)
+# 3. LIMIT 안써서 틀림 (아침에 일어나서 다시 풀었다고해도 문제는 다시읽어라.)
