464a1af2bbfb64931e9538bb8480106856d6e90d
financyeol <151622822+financyeol@users.noreply.github.com>
2024-01-13 14:10:27 +0900

    [level Lv.3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+
+
+SELECT i.name
+     , i.datetime
+FROM animal_ins i
+LEFT JOIN animal_outs o
+ON i.animal_id = o.animal_id
+WHERE o.datetime is null
+ORDER BY i.datetime ASC
+LIMIT 3;
