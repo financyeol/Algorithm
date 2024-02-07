2510cdff3adc52f1155832be6c236885663b0494
financyeol <151622822+financyeol@users.noreply.github.com>
2024-02-07 13:47:23 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM places
+WHERE host_id in 
+               (SELECT host_id 
+                FROM places 
+                GROUP BY host_id 
+                HAVING COUNT(host_id) >= 2)
+ORDER BY id
